4b4dc7c43adb522dd192d22b803de251392150f3
swan <163610015+swan-www@users.noreply.github.com>
2024-11-20 04:39:41 +0000

    revert the backend source files from the translation header

--- aggregate.h
+++ aggregate.h
@@ -1,5 +1,3 @@
 #define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #include "cimgui.h"
 #include "generator/output/cimgui_impl.h"
-#include "backends/imgui_impl_sdl3.h"
-#include "backends/imgui_impl_sdlrenderer3.h"
